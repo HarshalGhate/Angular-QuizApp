f553fd0b46bf6e43acfa630d7af3b8e05943b995
Harshal <harshalghate3131@gmail.com>
2022-01-12 17:54:13 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { QuestionComponent } from './question/question.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { HttpClientModule } from '@angular/common/http';
+import { ChangeBgDirective } from './change-bg.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    QuestionComponent,
+    HeaderComponent,
+    ChangeBgDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AlertModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/question/question.component.ts
@@ -0,0 +1,132 @@
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { QuestionService } from '../service/question.service';
+
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.scss']
+})
+export class QuestionComponent implements OnInit {
+
+  public name:string="";
+  public questionList:any=[];
+  public currentQuestion:number=0;
+  public points:number=0;
+  public counter=60;
+  correctAnswer:number=0;
+  incorrectAnswer:number=0;
+  interval$:any;
+  progress:string="0";
+  isQuizCompleted:boolean=false;
+
+  constructor(private qservice:QuestionService) { }
+
+  ngOnInit(): void {
+    this.name=localStorage.getItem("name")!;
+    this.getAllQuestions();
+    this.startCounter();
+  }
+
+  getAllQuestions(){
+    this.qservice.getQuestionJson().subscribe(res=>{
+      console.log(res.questions);
+      this.questionList=res.questions;
+    })
+  }
+  //To Next Question
+  nextQuestion()
+  {
+    this.currentQuestion++;
+  }
+  //To Previous Question
+  previousQuestion()
+  {
+    this.currentQuestion--;
+  }
+  //answer check
+  answer(currentQno:number,option:any)
+  {
+    if(currentQno===this.questionList.length)  //display result
+    {
+      this.isQuizCompleted=true;
+      this.stopCounter();
+    }
+    if(option.correct)                      //ans = correct
+    {      
+      this.points+=10;
+      //this.points=this.points+10;
+      this.correctAnswer++;
+      setTimeout(() => {
+        this.currentQuestion++;
+        this.resetCounter();
+        this.getProgressPercent();     
+      }, 1000);    
+      
+
+    }
+    else                                  //ans= incorrect
+    {
+      setTimeout(() => {
+        this.currentQuestion++;
+        this.incorrectAnswer++;
+        this.resetCounter();
+      }, 1000);
+      
+      this.points-=10;
+      
+      //this.getProgressPercent();
+      
+    }
+  }
+  startCounter()
+  {
+    this.interval$=interval(1000).subscribe(val=>{
+      this.counter--;
+      if(this.counter===0)
+      {
+        this.currentQuestion++;
+        this.counter=60;
+        this.points-=10;
+      }
+    });
+    setTimeout(()=>{
+        this.interval$.unsubscribe();
+    },600000);  //10min
+  }
+
+  stopCounter()
+  {
+    this.interval$.unsubscribe();
+    this.counter=0;
+
+  }
+  resetCounter()
+  {
+      this.stopCounter();
+      this.counter=60;
+      this.startCounter();
+  }
+
+  resetQuiz()
+  {
+    this.resetCounter();
+    this.getAllQuestions();
+    this.points=0;
+    this.counter=60;
+    this.currentQuestion=0;
+    this.progress="0";
+
+  }
+
+  getProgressPercent()
+  {
+    if(this.correctAnswer)
+    this.progress=((this.currentQuestion/this.questionList.length)*100).toString();
+    return this.progress;
+    
+  }
+ 
+
+
+}
